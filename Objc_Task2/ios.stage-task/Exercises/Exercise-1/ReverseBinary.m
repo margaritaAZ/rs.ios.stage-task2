4a1390742a07a6f8258fdf71dbb78c78d6824948
margaritaAZ <45636462+margaritaAZ@users.noreply.github.com>
2021-05-14 17:56:13 +0300

    feat:implement tasks 1-4 solutions

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,26 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSMutableArray<NSNumber*> *binaryNumberArray = [[NSMutableArray alloc] initWithCapacity:8];
+    NSInteger dividend = n;
+    
+    while (dividend >= 2) {
+        NSNumber *rest = [NSNumber numberWithInt:(dividend % 2)];
+        [binaryNumberArray addObject:rest];
+        dividend = dividend / 2;
+    }
+    [binaryNumberArray addObject:[NSNumber numberWithInteger:dividend]];
+    
+    while (binaryNumberArray.count < 8) {
+        [binaryNumberArray addObject:[NSNumber numberWithInteger:0]];
+    }
+
+    NSInteger newN = 0;
+    for (int i = 0; i < binaryNumberArray.count; i++) {
+        if (binaryNumberArray[i].intValue == 1) {
+            newN += pow(2, (binaryNumberArray.count - 1 - i));
+        }
+    }
+    
+    return newN;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,40 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    if (![array.firstObject isKindOfClass:[NSArray class]]) {
+        return @[];
+    }
+    
+        NSMutableArray *arrayOfNum = [[NSMutableArray alloc] init];
+        NSMutableArray *arrayOfStrings = [[NSMutableArray alloc] init];
+    
+        for (NSArray<NSString*> *subArray in array) {
+            if ([subArray.firstObject isKindOfClass:[NSString class]]) {
+                [arrayOfStrings addObjectsFromArray:subArray];
+                continue;
+            }
+            if ([subArray.firstObject isKindOfClass:[NSNumber class]]) {
+                [arrayOfNum addObjectsFromArray:subArray];
+                continue;
+            }
+        }
+    
+        NSMutableArray<NSArray *> *resultedArray = [[NSMutableArray alloc] init];
+        if (arrayOfNum.count != 0 && arrayOfStrings.count != 0) {
+            [resultedArray addObject:[arrayOfNum sortedArrayUsingSelector:@selector(compare:)]];
+            NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO selector:@selector(caseInsensitiveCompare:)];
+            [resultedArray addObject:[arrayOfStrings sortedArrayUsingDescriptors:@[sort]]];
+        } else if (arrayOfNum.count == 0 && arrayOfStrings.count != 0) {
+            NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES selector:@selector(caseInsensitiveCompare:)];
+            [resultedArray addObjectsFromArray:[arrayOfStrings sortedArrayUsingDescriptors:@[sort]]];
+        } else if (arrayOfNum.count != 0 && arrayOfStrings.count == 0){
+            [resultedArray addObjectsFromArray:[arrayOfNum sortedArrayUsingSelector:@selector(compare:)]];
+        } else {
+            return @[];
+        }
+    
+    return resultedArray;
 }
 
 @end
